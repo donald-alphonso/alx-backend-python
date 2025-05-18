10b5386fd056785c7fa331f1f764420f56b2064f
donald.alphonso <donaldalphonso11@gmail.com>
2025-05-18 10:49:40 +0200

    Task 2 - batch process users from SQL in batches with age > 25

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,43 +1,26 @@
 import mysql.connector
 
-def stream_users_in_batches(batch_size=25):
-    con = None
-    cursor = None
-    try:
-        conn = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='',
-            database='ALX_prodev'
-        )
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM user_data")
+def stream_users_in_batches(batch_size):
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='ALX_prodev'
+    )
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
 
-        while True:
-            batch = cursor.fetchmany(batch_size)
-            if not batch:
-                break
-            yield batch
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
 
-    except mysql.connector.Error as err:
-        print(f"Database error: {err}")
-        yield from []
+    cursor.close()
+    conn.close()
 
-    finally:
-        try :
-            if cursor:
-                cursor.close()
-            if conn:    
-                conn.close()
-        except
-            pass
-
-def batch_processing(batch_size=25):
-    try:
-        for batch in stream_users_in_batches(batch_size):
-            for user in batch:
-                if user['age'] > 25:
-                    print(user)
-
-    except Exception as e:
-        print(f"Error during batch processing: {e}")
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
