932bf2d49f8dbcc80542d5e23403a122c3fd4fcb
donald.ahossi <donaldalphonso11@gmail.com>
2025-05-24 14:41:31 +0200

    Adding @log_queries

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -33,3 +33,15 @@ def log_queries(func):
         return func(*args, **kwargs)
 
     return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
