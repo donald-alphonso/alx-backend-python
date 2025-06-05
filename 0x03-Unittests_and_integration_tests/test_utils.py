b76e748ec713f23f4c2d6ac76b07c3465d015600
donald.ahossi <donaldalphonso11@gmail.com>
2025-06-05 20:08:03 +0200

    remove space

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-"""
-Unittest for utils.access_nested_map
-"""
+"""Unittest for utils.access_nested_map"""
 
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    Test case for access_nested_map
-    """
+    """Test case for access_nested_map"""
 
     @parameterized.expand([
         ({"a", 1},("a",), 1),
@@ -18,9 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Test that access_nested_map returns the expected result
-        """
+        """Test that access_nested_map returns the expected result"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
         
     @parameterized.expand([
@@ -28,9 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), "b"),
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected_key):
-        """
-        Test that access_nested_map raises KeyError with message.
-        """
+        """Test that access_nested_map raises KeyError with message."""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), f"'{expected_key}'")
